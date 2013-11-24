29e2d9fc5952f587e16591ab7a1b124fe550a98b
adamdharrington <adamdharrington@gmail.com>
2013-11-24 12:03:10 +0000

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thoughtbubbles::Application.config.secret_token = 'f98223b99a713bbf63cde90953d044ff584cd7e60a87c7f79ee6f16a6a2be23d8e91fe6b58ee125915fb4262e8337f7c8e093c593004b559c50ee77e904e333d'
